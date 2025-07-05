5117cb82bc4c4f122ecb27a640c5e111bed63f4b
jatin yadav <jatinyadavsekwal88@gmail.com>
2025-07-05 23:03:40 +0530

    implementation of bookmark retrevial with catogory as query params

--- src/bookmark/index.ts
+++ src/bookmark/index.ts
@@ -67,11 +67,11 @@ bookmark.get("/", async ({ jwt, cookie: { auth } }) => {
   }
 });
 
-bookmark.post(
-  "/add",
-  async ({ body: { catogory, link }, jwt, cookie: { auth } }) => {
+bookmark.get(
+  "/:catogory",
+  async ({ params: { catogory }, jwt, cookie: { auth } }) => {
     try {
-        //verifying user
+      //verifying user
       const payload = await jwt.verify(auth?.value);
       const userId = (
         await db.select().from(Users).where(eq(Users.email, payload.email))
@@ -84,7 +84,44 @@ bookmark.post(
       }
       //check if catogory already exist
       //if exist then directly add
+      const data = await db.query.Catogory.findMany({
+        where: (Catogory, { eq, and }) =>
+          and(eq(Catogory.userId, userId), eq(Catogory.name, catogory)),
+        with: {
+          bookmarks: true,
+        },
+      });
+      if (data.length === 0) {
+        return {
+          message: "No bookmarks in this catogory yet",
+        };
+      }
+      //cleaing hte received data and give relevant data
+      type BookmarkResult = {
+        [key: string]: string[];
+      };
 
+      const result: BookmarkResult = {};
+
+  
+        const key = data[0]?.name;
+        if (key === undefined) return "ERROR";
+
+        const bookmarkLength = data[0]?.bookmarks.length;
+        if (bookmarkLength === undefined) return "ERROR";
+
+        for (let j = 0; j < bookmarkLength; j++) {
+          if (!result[key]) {
+            result[key] = [];
+          }
+          const link = data[0]?.bookmarks[j]?.link;
+          if (link) {
+            result[key].push(link);
+          }
+        }
+      
+
+      return result;
     } catch (error) {
       console.log(error);
       return {
@@ -93,9 +130,8 @@ bookmark.post(
     }
   },
   {
-    body: t.Object({
+    params: t.Object({
       catogory: t.String(),
-      link: t.String(),
     }),
   }
 );
